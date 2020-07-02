b54c93f5e7c45e296f6f631acdc405a4b444bc52
hanssenstijn <hanssenstijn@gmail.com>
2020-07-02 19:47:14 +0200

    seperate function into other file

--- src/sketch.js
+++ src/sketch.js
@@ -6,11 +6,6 @@ function make2DArray(cols, rows) {
   }
 }
 
-function Cell() {
-  this.bee = true;
-  this.revealed = true;
-}
-
 var grid;
 var cols = 20;
 var rows = 20;
@@ -23,11 +18,15 @@ function setup() {
       grid[i][j] = new Cell();
 
     }
-
-
   }
 }
 
 function draw() {
+  background(0);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
 
+    }
+  }
 }
