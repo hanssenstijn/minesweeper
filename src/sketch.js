ef062689ba4aa9c7c2e5c57ad85928b4f71d82a5
hanssenstijn <hanssenstijn@gmail.com>
2020-07-02 08:57:44 +0200

    check whats happening with new Array  function

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,24 @@
 
-function Cell() {
-
+function make2DArray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
 }
 
+function Cell() {
+  this.bee = true;
+  this.revealed = true;
+}
+
+var grid;
+var cols = 20;
+var rows = 20;
+
 function setup() {
+  createCanvas(200, 200);
+  grid = make2DArray(cols, rows);
+
 
 }
 
